af3737afcfe580dfaebf530560402e0388a30f7f
Théo Garampon <theo.garampon1@gmail.com>
2025-04-10 11:28:50 +0200

    test config

--- gourmet-recipes-app/src/App.jsx
+++ gourmet-recipes-app/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         </Routes>
         <ToastContainer
           position="top-right"
-          autoClose={3000}
+          autoClose={1500}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
